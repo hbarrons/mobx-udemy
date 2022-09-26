f9e88ccff3553dec641977ab992268a0441681ec
Howard Barrons <barronsh@locustec.com>
2022-09-26 09:23:21 -0400

    successfully create mobx observable object

--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,25 @@
+import {observable} from "mobx";
+
 console.log('it works');
+
+let person = observable({
+    firstName: 'MobX',
+    lastName: 'Course'
+})
+
+console.log('our person', person)
+
+class Person {
+    @observable
+    firstName: string;
+
+    constructor(name: string) {
+        this.firstName = name;
+    }
+}
+
+const newPerson = new Person('New Name')
+
+console.log('new Person', person)
 
 export {};
