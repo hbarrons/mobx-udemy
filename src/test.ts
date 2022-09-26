20f91421291b3334da58799cd3fcd80306599e12
Howard Barrons <barronsh@locustec.com>
2022-09-26 15:06:13 -0400

    add observable class

--- src/test.ts
+++ src/test.ts
@@ -1,25 +1,46 @@
-import {observable} from "mobx";
-
-console.log('it works');
-
-let person = observable({
-    firstName: 'MobX',
-    lastName: 'Course'
-})
-
-console.log('our person', person)
+import {action, autorun, observable, runInAction} from "mobx";
 
 class Person {
-    @observable
-    firstName: string;
+    @observable firstName: string = '';
+    @observable lastName: string = '';
+    @observable age: number = 15;
+    @observable isAlive: boolean = true;
 
-    constructor(name: string) {
+    constructor(name: string, lastName: string) {
         this.firstName = name;
+        this.lastName = lastName;
+    }
+
+    @action
+    updateFullName(name: string, lastName: string){
+        this.firstName = name;
+        this.lastName = lastName
+    }
+
+    @action
+    setAge(age: number) {
+        this.age = age
     }
 }
 
-const newPerson = new Person('New Name')
+const newPerson = new Person('Matt', 'Damon')
 
-console.log('new Person', person)
+newPerson.setAge(50)
+
+autorun(async () => {
+    console.log(`Person Name Is: ${newPerson.firstName} ${newPerson.lastName} (${newPerson.age})`);
+    console.log(newPerson)
+});
+
+runInAction(() => {
+    newPerson.firstName = 'Will';
+    newPerson.lastName = 'Hunting'
+    newPerson.isAlive = false;
+})
+
+newPerson.setAge(30)
+// newPerson.updateFullName('Will', 'Hunting')
+
+console.log(newPerson)
 
 export {};
