f450f723c77c97acbae18c7f7804b001595744a7
Howard Barrons <hbarrons@gmail.com>
2022-09-27 14:28:31 -0400

    import and utilize oberserver method

--- src/reacttest.tsx
+++ src/reacttest.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import ToDo from "./stores/data/todos/todo";
 import TodoStore from "./stores/data/todos/todo-store";
-import {Observer, useObserver} from "mobx-react";
+import {Observer, useObserver, observer} from "mobx-react";
 
 
 const newTodo = new ToDo('New Todo', 99, new TodoStore())
 
-const Test = () => {
-    return useObserver(()=>
+const Test = observer(() => {
+    console.log(newTodo.name);
+    return (
         <div>
             <div>{newTodo.name}</div>
             <button onClick={()=>newTodo.updateName('first name')}>First Name</button>
             <button onClick={()=>newTodo.updateName('last name')}>Last Name</button>
         </div>
     )
-};
+});
 
 export default Test;
