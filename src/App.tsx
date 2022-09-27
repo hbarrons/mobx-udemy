78d063818ac5788736628a34c615a77287ce2121
Howard Barrons <hbarrons@gmail.com>
2022-09-27 14:49:35 -0400

    render mobx todo list to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import Test from './reacttest';
+import {useStore} from "./stores/Helper/use-store";
 
 
 
 function App() {
-  return (
+    const {dataStores: {todoStore}} = useStore()
+    return (
     <div className="App">
-        <Test />
+        {todoStore.todolist.map(todo => <div key={todo.id}>{todo.name}</div>)}
     </div>
-  );
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import './test'
 import './todo-service'
 import RootStore from "./stores/root-store";
+import {createStore} from "./stores/Helper/create-store";
+import {StoreProvider} from "./stores/Helper/store-context";
 
-const rootStore = new RootStore();
+const rootStore = createStore()
 
-//create 4 users
-rootStore.dataStores.usersStore.addUser('Howard')
-rootStore.dataStores.usersStore.addUser('Nicole')
-rootStore.dataStores.usersStore.addUser('Shelbie')
-rootStore.dataStores.usersStore.addUser('Sam')
-
-//get user
-const newUser = rootStore.dataStores.usersStore.getUser('Howard')
-
-//add todos to the user
-rootStore.dataStores.todoStore.addToDo('Go to the gym', newUser.id);
-rootStore.dataStores.todoStore.addToDo('Learn mobx', newUser.id)
-
-//remove user
-rootStore.dataStores.usersStore.removeUser('Howard')
+rootStore.dataStores.todoStore.addToDo('Connect MobX to React', 99)
+rootStore.dataStores.todoStore.addToDo('Finish Udemy Course', 99)
+rootStore.dataStores.todoStore.addToDo('Learn EIM mobx Code', 99)
 
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(
+    <StoreProvider value={rootStore}>
+        <App />
+    </StoreProvider>
+    , document.getElementById('root')
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
