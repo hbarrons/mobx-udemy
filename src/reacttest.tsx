0c680168c90fd9ee6d697b8d3f7db572b9ec2f05
Howard Barrons <hbarrons@gmail.com>
2022-09-27 14:22:02 -0400

    import and use Observer from mobx-react

--- src/reacttest.tsx
+++ src/reacttest.tsx
@@ -1,9 +1,18 @@
-// @ts-ignore
 import React from "react";
+import ToDo from "./stores/data/todos/todo";
+import TodoStore from "./stores/data/todos/todo-store";
+import {Observer, useObserver} from "mobx-react";
+
+
+const newTodo = new ToDo('New Todo', 99, new TodoStore())
 
 const Test = () => {
     return (
-        <div>Test</div>
+        <div>
+            <Observer>{()=> <div>{newTodo.name}</div>}</Observer>
+            <button onClick={()=>newTodo.updateName('first name')}>First Name</button>
+            <button onClick={()=>newTodo.updateName('last name')}>Last Name</button>
+        </div>
     )
 };
 
