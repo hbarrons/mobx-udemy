09be5d0c673973c1d1e9dae944445250bee94046
Howard Barrons <hbarrons@gmail.com>
2022-09-28 09:05:24 -0400

    add functionality to add and remove users

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
+import {useStore} from "../stores/Helper/use-store";
+import {observer} from "mobx-react";
 
 
 const UserList = () => {
+    const {dataStores: {usersStore}} = useStore();
+    const [text, setText] = useState('');
+
+    const addUser = () => {
+        if (text.length <= 2) {
+            alert('Enter a User Name');
+            return;
+        }
+
+        usersStore.addUser(text);
+        setText('')
+    }
 
     return (
         <div className="row">
             <div className="col-sm-4">
-                Users
+                <div className="input-group">
+                    <input className="form-group" type="text" value={text} onChange={e => setText(e.target.value)}/>
+                    <div className="input-group-append">
+                        <button className="btn btn-outline-secondary" onClick={addUser}>
+                            Add User
+                        </button>
+                    </div>
+                </div>
+                <ul className="list-group">
+                    {usersStore.collection.map(user => (
+                        <li className="list-group-item">
+                            <span>{user.name}</span>
+                            <button className="btn btn-danger" onClick={()=>usersStore.removeUser(user.name)}>Remove</button>
+                        </li>
+                    ))}
+                </ul>
             </div>
             <div className="col-sm-8">
                 Todos
@@ -15,4 +44,4 @@ const UserList = () => {
     )
 }
 
-export default UserList;
+export default observer(UserList);
